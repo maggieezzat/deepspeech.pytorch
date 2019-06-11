2d2547185a1d91a86726240095af2e18def028dd
Hamahmi <Hamahmi@yahoo.com>
2019-06-11 12:19:58 +0200

    comment error

--- data/data_loader.py
+++ data/data_loader.py
@@ -31,14 +31,14 @@ def load_audio(path):
 
 
 class AudioParser(object):
-    """
+    '''
     def parse_transcript(self, transcript_path):
         """
-        #:param transcript_path: Path where transcript is stored from the manifest file
-        #:return: Transcript in training/testing format
+        :param transcript_path: Path where transcript is stored from the manifest file
+        :return: Transcript in training/testing format
         """
         raise NotImplementedError
-    """
+    '''
     def parse_audio(self, audio_path):
         """
         :param audio_path: Path where audio is stored from the manifest file
