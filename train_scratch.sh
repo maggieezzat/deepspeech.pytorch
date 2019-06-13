3f79a9756253f43bbb6e89da143e355867be6b18
maggieezzat <maggieezzat9@gmail.com>
2019-06-13 10:46:09 +0200

    fixed a bug

--- train_scratch.sh
+++ train_scratch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python train.py \
+python -m multiproc train.py \
 --train-manifest "/home/GPUAdmin1/asr/german-speechdata-package-v2/train/train.csv" \
 --val-manifest "/home/GPUAdmin1/asr/german-speechdata-package-v2/train/test.csv" \
---epochs 20 \
+--epochs 40 \
 --checkpoint \
---checkpoint-per-batch 5 \
+--checkpoint-per-batch 50 \
 --save-folder "/home/GPUAdmin1/asr/ds_pytorch_chkpts" \
 --hidden-layers 3 \
 --hidden-size 700 \
