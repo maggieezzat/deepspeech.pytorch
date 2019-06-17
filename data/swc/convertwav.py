64a1234e3084baaf737817a6d633bfe6269bcb43
Hamahmi <Hamahmi@yahoo.com>
2019-06-17 15:21:04 +0200

    finalize swc

--- data/swc/convertwav.py
+++ data/swc/convertwav.py
@@ -1,6 +1,9 @@
 import os
-with open("data/swc/wav.txt") as f:
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(dir_path + "/wav.txt") as f:
     lines = f.readlines()
 lines = [l.strip() for l in lines]
 for line in lines:
-    os.system(line[16:-3] + "data/swc/" + line[0:15] + ".wav")
+    os.system(line[16:-3] + line[0:15] + ".wav")
+    os.system("sudo rm -r " + dir_path + "/" + "/".join(line[20:].split("/")[0:5]))
