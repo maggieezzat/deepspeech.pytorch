77622dd0804ecd32a9f4be7935bbf435697abd42
Mariam-Dessouki <mariam_mostafa44@yahoo.com>
2019-06-17 15:55:34 +0200

    fixed a bug

--- data/swc/convertwav.py
+++ data/swc/convertwav.py
@@ -2,10 +2,10 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 #Where to store the wav files created to be changed on the VM
-SWC_path = "~/asr/data"
+SWC_path = "~/asr/"
 with open(dir_path + "/wav.txt") as f:
     lines = f.readlines()
 lines = [l.strip() for l in lines]
 for line in lines:
-    os.system(line[16:-3]+"~/Desktop/swc/" + line[0:15] + ".wav")
+    os.system(line[16:-3]+SWC_path + line[0:15] + ".wav")
     os.system("rm -r " + dir_path + "/" + "/".join(line[20:].split("/")[0:5]))
