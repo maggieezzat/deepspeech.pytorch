aa50578dfca5561055ea35f99f2782a7b58369a0
Ubuntu <GPUAdmin1@GPUSrv1.ypnrqfvlc2yexlhyoqvjswe2vc.xx.internal.cloudapp.net>
2019-06-18 08:43:49 +0000

    convertwav.py

--- data/swc/convertwav.py
+++ data/swc/convertwav.py
@@ -1,14 +1,22 @@
+
+
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-os.chdir("~/asr/")
+dir_path = "~/asr/"
 
+#os.chdir(os.path.expanduser("~/asr/"))
+#print(os.getcwd())
+#os.chdir("/home/GPUAdmin1/asr/")
+#print(os.getcwd())
 #Where to store the wav files created to be changed on the VM
 SWC_path = "~/asr/SWCData/"
-with open(dir_path + "/wav.txt") as f:
+with open("wav.txt") as f:
     lines = f.readlines()
 lines = [l.strip() for l in lines]
 for line in lines:
+    #print(line[16:-3]+SWC_path+line[0:15]+".wav")
     os.system(line[16:-3]+SWC_path + line[0:15] + ".wav")
-    os.system("rm -r " + dir_path + "/" + "/".join(line[20:].split("/")[0:5]))
-#os.chdir(dir_path)
+    #exit(0)
+    #os.system("rm -r " + dir_path + "/".join(line[20:].split("/")[0:5]))
+
