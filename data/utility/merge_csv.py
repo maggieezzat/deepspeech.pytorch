c451826e5de285ad123d879c6116ee83a4b63ed2
Hamahmi <Hamahmi@yahoo.com>
2019-06-21 12:13:07 +0200

    NOTE

--- data/utility/merge_csv.py
+++ data/utility/merge_csv.py
@@ -2,15 +2,20 @@ import os
 import pandas as pd
 
 
-#NOTE: 2 ADDITIONAL COMMAS BETWEEN EACH UTTERANCE IS ADDED NO TIME TO DEBUG REMOVED MANUALLY
+# NOTE: at the begining of the csv the columns MUST have the same name to be merged correctly (i.e. the first line of all csvs to be merged must be the same)
 """
 #TRAIN
 rootdir = "C:/Users/MariamDesouky/Desktop/train_csvs"
 data = pd.concat([pd.read_csv(rootdir+ "/" + file) for file in os.listdir(rootdir) ],axis=0,sort=False)
-data.to_csv("C:/Users/MariamDesouky/Desktop/train.csv", index=False, sep=",")
+data.to_csv("C:/Users/MariamDesouky/Desktop/train.csv", index=False,encoding='utf-8-sig')
 """
 
-#TEST
-rootdir = "E:/csv/"  
-data = pd.concat([pd.read_csv(rootdir+ "/" + file) for file in os.listdir(rootdir) ],axis=0,sort=False)
-data.to_csv("E:/csv/o.csv", index=False, encoding='utf-8-sig')
+# TEST
+rootdir = "E:/csv/"
+data = pd.concat(
+    [pd.read_csv(rootdir + "/" + file) for file in os.listdir(rootdir)],
+    axis=0,
+    sort=False,
+)
+data.to_csv("E:/csv/o.csv", index=False, encoding="utf-8-sig")
+
