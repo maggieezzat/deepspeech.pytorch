dc79c125cbca5d3170e3a1f2c035bbe04711a2a8
maggieezzat <maggieezzat9@gmail.com>
2019-06-26 16:00:03 +0200

    common voice csv

---
+++ data/common_voice/cv_duration.py
@@ -0,0 +1,20 @@
+import os
+import wave
+import datetime
+from pydub import AudioSegment
+
+#"spoken_wikipedia_german","M-AILABS",
+datafolders = ["common_voice_de"]
+time = datetime.timedelta(milliseconds = 0)
+for dir in datafolders:
+     for subdir, dirs, files in os.walk("/speech/"+dir):
+         for file in files:
+             if(file.endswith(".wav")):
+                    file_dir = os.path.join(subdir, file)
+                    millis = len(AudioSegment.from_wav(file_dir))
+                    time += datetime.timedelta(milliseconds= millis)
+                    print(file,end='\r')
+         print("ended "+ subdir)
+
+print()
+print(time.seconds/3600 + time.days*24)
