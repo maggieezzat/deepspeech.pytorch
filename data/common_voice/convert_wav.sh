6c4dbc99604ba810eef5ef15148a848770d8ba70
Mariam-Dessouki <mariam_mostafa44@yahoo.com>
2019-06-27 11:53:16 +0000

    [convert_wavCV] info added while converting

--- data/common_voice/convert_wav.sh
+++ data/common_voice/convert_wav.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
-for i in /speech/common_voice_de/clips/*.mp3
-do
-    sox "$i" "/speech/common_voice_de/wav_files/$(basename -s .mp3 "$i").wav"
+foldername="/speech/common_voice_de/clips/"
+outputfolder="/speech/common_voice_de/wav_files/"
+FILECOUNT=$(find $foldername -type f | wc -l)
+count=1
+for filename in "/speech/common_voice_de/clips/"*; do
+    fn=$(basename -- "$filename")
+    echo -ne $count / $FILECOUNT '\r'
+    ((count++))
+    sox $foldername$fn -r 16000 -c 1 -b 16 "$outputfolder${fn%???}wav"	
 done
+echo "=-=-=-=-=-=Done=-=-=-=-=-="
