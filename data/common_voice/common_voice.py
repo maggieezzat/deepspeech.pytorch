b0c7493d9543f18a7adb5db021014903d4588a98
maggieezzat <maggieezzat9@gmail.com>
2019-06-27 12:09:57 +0200

    common voice

--- data/common_voice/common_voice.py
+++ data/common_voice/common_voice.py
@@ -90,7 +90,7 @@ def rename_utterances(root_dir = dir):
             src = os.path.join(valid_wav, line[1]+".wav")
             client_id = line[0]
             client_id = '042c5ba1d0cdfc6459d62c618596435a082082c96e4cd52079e01b23f338cd607928a3491763e5cdf7750105b640ab6eaa78aa9050ac7a0fd9234ad423b9cec6'
-            speaker = speakers_dict.get(client_id)
+            speaker = speakers_dict.get(100)
             print(speaker)
             #print(client_id)
             exit(0)
