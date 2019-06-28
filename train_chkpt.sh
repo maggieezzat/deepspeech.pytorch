72de7babf8a7349916f2067b8968643887b7c5df
Mariam-Dessouki <mariam_mostafa44@yahoo.com>
2019-06-28 16:40:34 +0200

    fixed training scripts

--- train_chkpt.sh
+++ train_chkpt.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 python -m multiproc train.py \
---visdom \
 --train-manifest "/home/GPUAdmin1/asr/train.csv" \
---val-manifest "/home/GPUAdmin1/asr/test.csv" \
+--val-manifest "/home/GPUAdmin1/asr/dev.csv" \
 --epochs 40 \
---learning-anneal 10 \
 --checkpoint \
---checkpoint-per-batch 50 \
---save-folder "/speech/test/" \
---continue-from "/speech/ds_pytorch_chkpts/$1" \
+--checkpoint-per-batch 200 \
+--save-folder "/speech/ds_pytorch_chkpts_600/" \
+--continue-from "/speech/ds_pytorch_chkpts_600/$1" \
 --hidden-layers 3 \
 --hidden-size 700 \
 --cuda \
---model-path "/speech/test/deepspeech_final.pth"
+--model-path "/speech/ds_pytorch_chkpts_600/deepspeech_final.pth"
--- train_scratch.sh
+++ train_scratch.sh
@@ -2,10 +2,10 @@
 
 python -m multiproc train.py \
 --train-manifest "/home/GPUAdmin1/asr/train.csv" \
---val-manifest "/home/GPUAdmin1/asr/test.csv" \
+--val-manifest "/home/GPUAdmin1/asr/dev.csv" \
 --epochs 40 \
 --checkpoint \
---checkpoint-per-batch 50 \
+--checkpoint-per-batch 200 \
 --save-folder "/speech/ds_pytorch_chkpts_600/" \
 --hidden-layers 3 \
 --hidden-size 700 \
