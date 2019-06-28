9f3f4640fccc42b94ab4a95acebc02e0dae3415f
Hamahmi <Hamahmi@yahoo.com>
2019-06-28 14:02:36 +0200

    debugging

--- data/common_voice/random_speakers.py
+++ data/common_voice/random_speakers.py
@@ -8,24 +8,43 @@ time = datetime.timedelta(milliseconds=0)
 root = "/speech/common_voice_de/common_voice_valid_wav.csv"
 speakers = []
 test = []
+dev = []
+train = []
 hours_so_far = 0
 hours_needed = 7
 speaker = ""
+test = 0
 with open(root, encoding="utf-8-sig") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     for row in csv_reader:
         wav_dir = row[0]
-        speaker = wav_dir.split("spk",1)[1].split("_",1)[0]
+        speaker = wav_dir.split("spk", 1)[1].split("_", 1)[0]
         speakers.append(speaker)
         millis = len(AudioSegment.from_wav(wav_dir))
         time += datetime.timedelta(milliseconds=millis)
-        hours_so_far += time.seconds/3600 +  time.days * 24
+        hours_so_far += time.seconds / 3600 + time.days * 24
+        if test == 0:
+            test.append(row)
+        elif test == 1:
+            dev.append(row)
+        else:
+            train.append(row)
         if hours_so_far >= hours_needed:
-            break
+            print()
+            print(test)
+            print()
+            test += 1
+            print(hours_so_far)
+            hours_so_far = 0
+            if test == 1:
+                hours_needed = 26
+            else:
+                hours_needed = 320
         print(hours_so_far, end="\r")
 
 print()
-print(len(set(speakers)))
-print(speakers[-1])
+print(len(test))
+print(len(dev))
+print(len(train))
 print("*******")
 
