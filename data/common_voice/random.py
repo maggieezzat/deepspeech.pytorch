b96837daa99a41c28409bf759525dd1c99faedcd
Hamahmi <Hamahmi@yahoo.com>
2019-06-28 13:40:19 +0200

    [random] checking

---
+++ data/common_voice/random.py
@@ -0,0 +1,26 @@
+import os
+import csv
+import wave
+import datetime
+from pydub import AudioSegment
+
+time = datetime.timedelta(milliseconds=0)
+root = "/speech/common_voice_de/common_voice_valid_wav.csv"
+speakers = []
+test = []
+hours_so_far = 0
+hours_needed = 7
+speaker = ""
+with open(root, encoding="utf-8-sig") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    for row in csv_reader:
+        wav_dir = row[0]
+        millis = len(AudioSegment.from_wav(wav_dir))
+        time += datetime.timedelta(milliseconds=millis)
+        hours_so_far += time.seconds/3600 +  time.days * 24
+        if hours_so_far >= hours_needed:
+            break
+        
+        print(hours_so_far, end="\r")
+print("*******")
+
