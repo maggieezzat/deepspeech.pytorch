69d89973f4c3f2e994acdb93a87de01c749b0ca1
Mariam-Dessouki <mariam_mostafa44@yahoo.com>
2019-07-01 21:41:04 +0200

    added file to know which path causes error

--- train_scratch.sh
+++ train_scratch.sh
@@ -11,3 +11,13 @@ python -m multiproc train.py \
 --hidden-size 700 \
 --cuda \
 --model-path "/speech/ds_pytorch_models_600/deepspeech_final.pth"
+
+
+Training Summary Epoch: [1]     Time taken (s): 24165   Average Loss 43.878
+100%|█████████████████████████████████████████████████████████████| 269/269 [05:47<00:00,  1.66it/s]
+Validation Summary Epoch: [1]   Average WER 67.733      Average CER 19.650
+Learning rate annealed to: 0.000273
+
+Training Summary Epoch: [1]     Time taken (s): 7283    Average Loss 35.449
+100%|█████████████████████████████████████████████████████████████| 269/269 [08:49<00:00,  1.08it/s]
+Validation Summary Epoch: [1]   Average WER 76.938      Average CER 27.503
