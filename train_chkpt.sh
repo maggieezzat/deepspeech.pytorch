2faee87ddc53db101dbcea7ff1fd0e7610d3b21e
Mariam-Dessouki <mariam_mostafa44@yahoo.com>
2019-07-08 08:55:54 +0000

    changed batch size and checkpoint per batch --maggie :D

--- train_chkpt.sh
+++ train_chkpt.sh
@@ -3,9 +3,10 @@
 python -m multiproc train.py \
 --train-manifest "/home/GPUAdmin1/asr/train.csv" \
 --val-manifest "/home/GPUAdmin1/asr/dev.csv" \
+--batch-size 14 \
 --epochs 40 \
 --checkpoint \
---checkpoint-per-batch 450 \
+--checkpoint-per-batch 150 \
 --save-folder "/speech/ds_pytorch_chkpts_lstm_5/" \
 --continue-from "/speech/ds_pytorch_chkpts_lstm_5/$1" \
 --hidden-layers 5 \
