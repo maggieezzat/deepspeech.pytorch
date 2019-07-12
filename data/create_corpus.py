67d275a1986afe3ec63b776799c7556c34dab908
Mariam-Dessouki <mariam_mostafa44@yahoo.com>
2019-07-12 10:14:33 +0200

    modified corpus creation script for lm

--- data/create_corpus.py
+++ data/create_corpus.py
@@ -2,9 +2,8 @@ import csv
 from clean_text import clean_sentence
 
 rootdir = "/data/home/GPUAdmin1/asr/"
-files = [rootdir+"train_csvs/M-AILABS_train.csv",rootdir+"train_csvs/cv_train.csv","/speech/german-speechdata-package-v2/SentencesAndIDs.cleaned.txt",
-rootdir+"test_csvs/cv_test.csv",rootdir+"dev_csvs/cv_dev.csv"]
-
+files = [rootdir+"train_csvs/M-AILABS_train.csv",rootdir+"train_csvs/cv_train.csv","/speech/german-speechdata-package-v2/SentencesAndIDs.cleaned.txt"]
+files_out = [rootdir+"test_csvs/cv_test.csv",rootdir+"dev_csvs/cv_dev.csv"]
 sentences = []
 for file_dir in files:
     if(".csv" in file_dir):
@@ -18,9 +17,18 @@ for file_dir in files:
                 sent = clean_sentence(line.split(" ",1)[1])
                 sentences.append(sent+"\n")
 
+sentences_out =[]
+for file_dir in files_out:
+    with open(file_dir) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                sentences_out.append(row[1] +"\n")
 
 
 sent_set = set(sentences)
 corpus = open("/data/home/GPUAdmin1/asr/corpus.txt","w")
-corpus.writelines(sent_set)
+for sent in sent_set:
+    if(sent in sentences_out):
+        continue
+    corpus.writeline(sent)
 
