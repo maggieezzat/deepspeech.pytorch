c261ad819647b87f29c0c416fe6e0ac4839d4e82
maggieezzat <maggieezzat9@gmail.com>
2019-07-15 19:50:23 +0200

    web_sent

--- data/web_sent_5M.py
+++ data/web_sent_5M.py
@@ -5,17 +5,19 @@ rootdir = "/lm_corpus/web_sent_5M/"
 sentences =[]
 i=1
 for file_name in os.listdir(rootdir):
-    if(file_name.endswith(".txt")):
-        #with open(rootdir+file_name, "r") as text:
-        #    c = 0
-        #    for line in text:
-        #        c+=1
-        #        print(str(c) + " / 1000000" , end = '\r')
-        #        sent = clean_sentence(line.split(" ",1)[1])
-        #        sentences.append(sent+"\n")
-        #print()
-        #print("File "+ str(i) +" done")
-        #i+=1
+    if(file_name.endswith("deu-at_web_2014_1M-sentences.txt")):
+        with open(rootdir+file_name, "r") as text:
+            c = 0
+            for line in text:
+                c+=1
+                print(str(c) + " / 1000000" , end = '\r')
+                if not line.split(" ",1)[1]:
+                        continue
+                sent = clean_sentence(line.split(" ",1)[1])
+                sentences.append(sent+"\n")
+        print()
+        print("File "+ str(i) +" done")
+        i+=1
         print(file_name + "\n")
 
 
@@ -23,4 +25,4 @@ for file_name in os.listdir(rootdir):
 sent_set = set(sentences)
 print(len(sent_set))
 corpus = open("/lm_corpus/web_sent_5M.txt","w")
-corpus.writelines(corpus)
+corpus.writelines(sent_set)
