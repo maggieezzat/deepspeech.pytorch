3849996980d44634b6e335f858e87a1ed212c99d
maggieezzat <maggieezzat9@gmail.com>
2019-07-16 09:40:21 +0200

    merge dewiki

--- data/dewiki_merge.sh
+++ data/dewiki_merge.sh
@@ -3,7 +3,7 @@
 root_dir=/lm_corpus/dewiki_nltk_segmented/
 wiki=/lm_corpus/dewiki.txt
 
-for dir in root_dir; do
+for dir in $root_dir; do
     for file in $(find $root_dir/$dir -maxdepth 1 -type f); do
         cat $file >> $wiki
     done
