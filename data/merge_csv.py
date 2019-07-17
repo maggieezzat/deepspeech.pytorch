71d9e20e39719a2fddcefe08b04e5a9216e61e72
maggieezzat <maggieezzat9@gmail.com>
2019-07-17 13:21:47 +0200

    modified merge_csv.py

--- data/merge_csv.py
+++ data/merge_csv.py
@@ -37,6 +37,5 @@ for dir in dirs:
         
     )
     directory = dir.split("_")
-    #data.to_csv("/data/home/GPUAdmin1/asr/"+directory[0]+".csv", index=False, header=False, encoding="utf-8-sig")
-    data.to_csv("/speech/"+directory[0]+".csv", index=False, header=False, encoding="utf-8-sig")
+    data.to_csv("/data/home/GPUAdmin1/asr/"+directory[0]+".csv", index=False, header=False, encoding="utf-8-sig")
     
