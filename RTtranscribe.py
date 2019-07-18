862328bcd661fefeec8cd6a8c25bac1b35938324
Hamahmi <Hamahmi@yahoo.com>
2019-07-18 12:57:33 +0200

    [RTtranscripe] import funcs

--- RTtranscribe.py
+++ RTtranscribe.py
@@ -14,49 +14,14 @@ from data.data_loader import SpectrogramParser
 from model import DeepSpeech
 import os.path
 import json
-
-
-def decode_results(model, decoded_output, decoded_offsets):
-    results = {
-        "output": [],
-        "_meta": {
-            "acoustic_model": {"name": os.path.basename(args.model_path)},
-            "language_model": {
-                "name": os.path.basename(args.lm_path) if args.lm_path else None
-            },
-            "decoder": {
-                "lm": args.lm_path is not None,
-                "alpha": args.alpha if args.lm_path is not None else None,
-                "beta": args.beta if args.lm_path is not None else None,
-                "type": args.decoder,
-            },
-        },
-    }
-
-    for b in range(len(decoded_output)):
-        for pi in range(min(args.top_paths, len(decoded_output[b]))):
-            result = {"transcription": decoded_output[b][pi]}
-            if args.offsets:
-                result["offsets"] = decoded_offsets[b][pi].tolist()
-            results["output"].append(result)
-    return results
-
-
-def transcribe(audio_path, parser, model, decoder, device):
-    spect = parser.parse_audio(audio_path).contiguous()
-    spect = spect.view(1, 1, spect.size(0), spect.size(1))
-    spect = spect.to(device)
-    input_sizes = torch.IntTensor([spect.size(3)]).int()
-    out, output_sizes = model(spect, input_sizes)
-    decoded_output, decoded_offsets = decoder.decode(out, output_sizes)
-    return decoded_output, decoded_offsets
+from transcribe import transcribe, decode_results
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="DeepSpeech transcription")
     parser = add_inference_args(parser)
     parser.add_argument(
-        "--audio-path", default="audio.wav", help="Audio file to predict on"
+        "--audio-path", default="audio.wav", help="Audio files to predict on"
     )
     parser.add_argument(
         "--offsets",
@@ -88,6 +53,7 @@ if __name__ == "__main__":
     parser = SpectrogramParser(model.audio_conf, normalize=True)
 
     audio_files = (args.audio_path).split(",")
+    print("Starting")
     for af in audio_files:
         decoded_output, decoded_offsets = transcribe(af, parser, model, decoder, device)
         print(json.dumps(decode_results(model, decoded_output, decoded_offsets)))
