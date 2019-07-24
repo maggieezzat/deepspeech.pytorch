0e882b97984d1e3f3ddfb6f460f7b29ccf2e3b5c
Hamahmi <Hamahmi@yahoo.com>
2019-07-24 13:45:13 +0200

    [movie] python dir fixed

--- data/movies/run_movie.sh
+++ data/movies/run_movie.sh
@@ -3,5 +3,5 @@ wavfolder=/speech/movieswavs/
 for foldername in /speech/Movies/*/; do
     fn=$(basename -- "$foldername")
     mkdir -p $wavfolder$fn
-    python3 movie_csv.py --dirmovie $foldername --dircsv "/speech/movieswavs/movies.csv" --dirseg $wavfolder$fn
+    python3 /data/home/GPUAdmin1/asr/deepspeech.pytorch/data/movies/movie_csv.py --dirmovie $foldername --dircsv "/speech/movieswavs/movies.csv" --dirseg $wavfolder$fn
 done
