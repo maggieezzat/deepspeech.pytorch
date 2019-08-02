b96611a38f16c26643cf5079aaa1542889918fc7
Mariam-Dessouki <mariam_mostafa44@yahoo.com>
2019-08-02 15:03:58 +0200

    lm rescoring

---
+++ rescoring.py
@@ -0,0 +1,23 @@
+import kenlm
+
+
+def rescore_sent(utterances, no_paths = 100 ,rescoring_lm = "/lm_corpus/train_mary.binary")
+    """
+    takes as an input the output sentences from the beam search lm and scores them with a bigger lm
+    to return the most probable one
+    """
+    rescoring_results = [[] for j in range(len(utterances))]
+    model = kenlm.LanguageModel(rescoring_lm)
+    for utterance in range(len(utterances)):
+        maxScore = float("-inf")
+        maxIndex = -1
+        print("rescore")
+        for i in range(min(no_paths,len(utterances[utterance]))):
+            score =   model.score(utterances[utterance][i])
+            if(score > maxScore):
+                maxScore = score
+                maxIndex = i
+        rescoring_results[utterance].append(utterances[utterance][maxIndex])
+            
+    return rescoring_results
+    
