b020b7fb5ffaa9ff7d4f0cf6289acbf40c844d9c
Mariam-Dessouki <mariam_mostafa44@yahoo.com>
2019-08-02 15:20:50 +0200

    audio path

--- decoder.py
+++ decoder.py
@@ -24,6 +24,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='DeepSpeech decoder')
 parser = add_inference_args(parser)
+parser.add_argument('--audio-path', default='audio.wav',
+                        help='Audio file to predict on')
 parser = add_decoder_args(parser)
 args = parser.parse_args()
 class Decoder(object):
