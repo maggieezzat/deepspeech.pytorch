d25eb47ac258356dacf315205ba5a789b4d3dc53
Mariam-Dessouki <mariam_mostafa44@yahoo.com>
2019-08-06 12:53:08 +0200

    encoding

--- transcribe_tuda.py
+++ transcribe_tuda.py
@@ -4,9 +4,10 @@ import time
 #given that the worker and server are already running
 
 if(not os.path.exists("/speech/missing.txt")):
-    with open("/home/GPUAdmin1/asr/test.csv","r",encoding="utf-8-sig") as csv_file:
+    with open("/home/GPUAdmin1/asr/test.csv","rb") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
+            row = row.decode("utf-8-sig")
             os.system("bash /data/home/GPUAdmin1/kaldi/egs/tuda/decode_wav.sh "+ row[0])
             #give the server some time
             #time.sleep(0.25)
