19afe4ba8499358839560a6d1e6357701b890e5f
maggieezzat <maggieezzat9@gmail.com>
2019-08-07 12:14:46 +0200

    asr_correction.py for the transformer

--- transformer/commands.sh
+++ transformer/commands.sh
@@ -38,7 +38,17 @@ echo "ürer aun nur kut wi die flügel eilnes noch nicht fluge sperrerlings so h
 echo "zu aurbeutetdu sie vorsichtig wie unter eimer glausglocke geng sie abends mit ihrer diemnerin auf dem hohen holzschund zum theautur" >> $DECODE_FILE
 #so arbeitete sie vorsichtig wie unter einer glasglocke ging sie abends mit ihrer dienerin auf den hohen holzschuhen zum theater
 
+Inference results INPUT: rer aun nur kut wi die flgel eilnes noch nicht fluge sperrerlings so has sie auft der markte und bereite tie iren tieg
+Inference results OUTPUT: ihre arme kurz wie die flügel eines noch nicht flüggen sperlings sass sie auf der matte und bereitete ihren tee
 
+Inference results INPUT: zu aurbeutetdu sie vorsichtig wie unter eimer glausglocke geng sie abends mit ihrer diemnerin auf dem hohen holzschund zum theautur
+Inference results OUTPUT: so arbeitete sie vorsichtig wie unter einer glasglocke ging sie abends mit ihrer dienerin auf den hohen holzschuhen zum theater
+
+Inference results INPUT: um zulegen gttlichen schnhaiten der fergnglickeit getzil zu werden ie hal war biegsam wie alne reie fel dur
+Inference results OUTPUT: um zu den göttlichen schönheiten der vergänglichkeit gezählt zu werden ihr hals war biegsam wie eine reiherfeder
+
+Inference results INPUT: hanake hatte allem korperschmuk den einem japanisches medbchen stzhen drcpennd und liegend zeugen muss
+Inference results OUTPUT: hanake hatte allen körperschmuck den ein japanisches mädchen sitzend trippelnd und liegend zeigen muss
 
 t2t-decoder \
   --data_dir=$DATA_DIR \
@@ -48,5 +58,5 @@ t2t-decoder \
   --output_dir=~/t2t_train/asr_correction \
   --decode_hparams="beam_size=$BEAM_SIZE,alpha=$ALPHA" \
   --decode_from_file=$DECODE_FILE \
-  --decode_to_file=~/asr/asr_correction_decoder.txt \
+  --decode_to_file=/data/home/GPUAdmin1/asr/asr_correction_decoder.txt \
   --t2t_usr_dir=$USR_DIR
