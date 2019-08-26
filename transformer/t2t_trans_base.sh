06a94823171075bca419e200e28e360b0d39a917
maggieezzat <maggieezzat9@gmail.com>
2019-08-26 16:01:34 +0200

    transformer base

--- transformer/t2t_trans_base.sh
+++ transformer/t2t_trans_base.sh
@@ -2,8 +2,8 @@ USR_DIR=/data/home/GPUAdmin1/asr/deepspeech.pytorch/transformer/
 PROBLEM=asr_correction
 MODEL=transformer
 HPARAMS=transformer_base_single_gpu
-DATA_DIR=$HOME/t2t_data
-OUT_DIR=/t2t_train/trans_base_single_gpu
+DATA_DIR=$HOME/t2t_data/5gram_single_spk_dev/
+OUT_DIR=/t2t_train/transformer_base_3M
  
 CUDA_VISIBLE_DEVICES=0 t2t-trainer \
 --data_dir=$DATA_DIR \
